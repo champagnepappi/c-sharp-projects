1ddd6fe83390216913aba995c0fc3e40c62435f5
kevin <kevinochieng548@gmail.com>
2016-12-01 01:36:13 +0300

    define Acceptdetails, GetArea and display methods

---
+++ private.cs
@@ -0,0 +1,38 @@
+using System;
+namespace Loops
+{
+    class Rectangle
+    {
+        //member variables
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+
+        }
+    }
+    class ExecuteRectangle
+    {
+        static void Main(string[]args)
+        {
+
+        }
+    }
+}
