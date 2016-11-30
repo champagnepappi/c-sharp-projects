7a9dd21738695dddde3a99ff0118a13fee28e887
kevin <kevinochieng548@gmail.com>
2016-12-01 02:09:30 +0300

    demonstrate method call

---
+++ methodcall.cs
@@ -0,0 +1,30 @@
+using System;
+namespace CalculatorApplication
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            int result;
+            if(num1>num2)
+                result = num1;
+            else
+                result = num2;
+        }
+
+        static void Main(string[]args)
+        {
+            int a =100;
+            int b = 200;
+            int ret;
+
+            NumberManipulator n = new NumberManipulator();
+
+            ret = n.FindMax(a,b);
+
+            Console.WriteLine("Max value is: {0}", ret);
+            Console.ReadLine();
+
+        }
+    }
+}
