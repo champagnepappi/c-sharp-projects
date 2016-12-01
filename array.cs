87e0b3b7e59315091ead26c297c3905fd779ebab
kevin <kevinochieng548@gmail.com>
2016-12-01 10:24:08 +0300

    use foreach to loop through arrays

---
+++ array.cs
@@ -0,0 +1,28 @@
+using System;
+namespace ArrayApplication
+{
+    class MyArray
+    {
+        static void Main(string[]args)
+        {
+            int [] n = new int[10];
+            
+            /*initialize elements of an array*/
+            for(int i=0;i<10;i++)
+            {
+                n[i] = 100;
+            }
+
+            /*output each array element's value*/
+            foreach(int j in n)
+            {
+                int i = j -100;
+                Console.WriteLine("Element[{0}] = {1}", i,j);
+                i++;
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
