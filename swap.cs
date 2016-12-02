af0e35b4274611ad6d728265884d80c69a70e735
kevin <kevinochieng548@gmail.com>
2016-12-02 20:38:40 +0300

    define main where class is instantiated

--- swap.cs
+++ swap.cs
@@ -11,5 +11,21 @@ namespace CalculatorApplication
             y = temp; //put temp into y
 
         }
+        static void Main(string[]args)
+        {
+            NumberManipulator n = new NumberManipulator();
+                /* local variable declaration*/
+                int a = 100;
+            int b = 200;
+
+            Console.WriteLine("Before swap, value of a: {0}", a);
+            Console.WriteLine("Before swap, value of b: {0}", b);
+
+            /*call function to swap the values*/
+            n.swap(ref a, ref b);
+            Console.WriteLine("After swap, value of a: {0}", a);
+            Console.WriteLine("After swap, value of b: {0}", b);
+            Console.ReadLine();
+        }
     }
 }
