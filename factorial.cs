caaf5e4352c875607abbe74cb42ad5874e2fd304
kevin <kevinochieng548@gmail.com>
2016-12-03 13:45:03 +0300

    find factorial of 6,7 and 8

--- factorial.cs
+++ factorial.cs
@@ -17,5 +17,15 @@ namespace CalculatorApplication
                 return result;
             }
         }
+        static void Main(string[]args)
+        {
+            NumberManipulator n = new NumberManipulator();
+            //call the factorial method
+            Console.WriteLine("Factorial of 6 is : {0}", n.factorial(6));
+            Console.WriteLine("Factorial of 7 is : {0}", n.factorial(7));
+            Console.WriteLine("Factorial of 8 is : {0}", n.factorial(8));
+
+            Console.ReadLine();
+        }
     }
 }
