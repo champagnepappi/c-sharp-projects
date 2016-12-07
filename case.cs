dba907f20484c91b9a611499fa73cb4f7bd543ba
kevin <kevinochieng548@gmail.com>
2016-12-07 22:51:23 +0300

    declare local variable grade

---
+++ case.cs
@@ -0,0 +1,13 @@
+using System;
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            /* local variable definition*/
+             char grade = 'B';
+
+        }
+    }
+}
