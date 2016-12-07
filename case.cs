843f5aca8aaf31af1201e799d8f33bafeab0068d
kevin <kevinochieng548@gmail.com>
2016-12-07 22:58:00 +0300

    define switch case

--- case.cs
+++ case.cs
@@ -8,6 +8,27 @@ namespace DecisionMaking
             /* local variable definition*/
              char grade = 'B';
 
+             switch(grade)
+             {
+                 case 'A':
+                     Console.WriteLine("Excellent!");
+                     break;
+                 case 'B':
+                     Console.WriteLine("Average Performance");
+                     break;
+                 case 'C':
+                     Console.WriteLine("Well Done");
+                     break;
+                 case 'D':
+                     Console.WriteLine("Ypu passed");
+                     break;
+                 case 'E':
+                     Console.WriteLine("Better try again");
+                     break;
+                 default:
+                     Console.WriteLine("Invalid grade");
+                     break;
+             }
         }
     }
 }
