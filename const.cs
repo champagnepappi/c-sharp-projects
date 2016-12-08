758f709462477ad154fd225fd73dd943f0458105
kevin <kevinochieng548@gmail.com>
2016-12-08 13:37:47 +0300

    declare and use constants

---
+++ const.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            const double pi = 3.14159; //constant declaration
+            double r;
+
+            Console.WriteLine("Enter radius:  ");
+
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi*r*r;
+
+            Console.WriteLine("Radius: {0}, Area: {1}", r,areaCircle);
+            Console.ReadLine();
+        }
+    }
+}
